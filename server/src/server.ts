d21724db764bc104cb6f55a0d8d16d6e30d4e651
Rodolfo Augusto <r.augusto1892@gmail.com>
2020-07-27 14:36:42 -0300

    Exemplos do express

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,29 @@
-import express from 'express'
+import express, { response } from 'express'
 
 const app = express();
 
-app.get('/users', function(req, res){
-  res.json([
-    'Rodolfo',
-    'Michele',
-    'Alana',
-    'Ellen'
-  ])
+app.use(express.json())
+
+const users = ['Rodolfo', 'Michele', 'Alana', 'Ellen']
+
+app.get('/users', function (req, res) {
+  const search = String(req.query.search)
+  const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+ return res.json(filteredUsers)
+});
+
+app.get('/users/:id', (req, res)=>{
+  return res.json(users[parseInt(req.params.id)])
+})
+
+app.post('/users', (req, res) => {
+  const data = req.body;
+
+  const newUser = {
+    nome: data.name,
+    email: data.email
+  }
+  return res.json(newUser)
 })
 
 app.listen(3333)
